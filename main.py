86f32715623fe7309516489471ac38517461ff21
JMHumphreys <jmh09r@my.fsu.edu>
2023-05-07 13:44:27 -0600

    Update main.py

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ logging.config.fileConfig("logging.conf")
 
 def setup_validation_run_for_pull_request(project_dir: str) -> ValidationRun:
     # load config file
-    config = "https://github.com/JMHumphreys/mod-eval-hub/blob/main/covid-validation-config.json"
+    config = "os.path.join(project_dir, "covid-validation-config.json")
     f = open(config)
     config_dict = json.load(f)
     f.close()
